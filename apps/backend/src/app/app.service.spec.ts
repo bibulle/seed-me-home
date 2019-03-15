78db52f6d9cd73497f53cb489c485456f3f13b3f
Martin, Eric (DI NB) - AF <eric.martin@airfrance.fr>
2019-03-15 13:21:03 +0100

    feat: add pre-push controls

--- apps/backend/src/app/app.service.spec.ts
+++ apps/backend/src/app/app.service.spec.ts
@@ -15,7 +15,7 @@ describe('AppService', () => {
 
   describe('getData', () => {
     it('should return "Welcome to backend!"', () => {
-      expect(service.getData()).toEqual({ message: 'Welcome to backend!' });
+      expect(AppService.getData()).toEqual({ message: 'Welcome to backend!' });
     });
   });
 });
