2cc6fdf1f0c31674856ad1159e998686df1300aa
Martin, Eric (DI NB) - AF <eric.martin@airfrance.fr>
2019-05-14 17:35:24 +0200

    feat: prepare for files list

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { AppService } from './app.service';
 import { RtorrentModule } from './rtorrent/rtorrent.module';
 import { ConfigService } from '../services/config/config.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { FilesController } from './files/files.controller';
+import { FilesService } from './files/files.service';
 
 @Module({
   imports: [RtorrentModule, AuthenticationModule],
-  controllers: [AppController],
+  controllers: [AppController, FilesController],
   providers: [
     AppService,
-    ConfigService
+    ConfigService,
+    FilesService
     //FtpSeedService
   ]
 })
